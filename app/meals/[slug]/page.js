2af0ce6fd6d2302ee784938ac439b8f37ff39e6d
Alex Brilevski <brilevski.alex@gmail.com>
2026-01-11 10:39:58 +0300

    Basic structure of pages was implemented

---
+++ app/meals/[slug]/page.js
@@ -0,0 +1,11 @@
+const MealPage = async ({ params }) => {
+  const { slug } = await params;
+  return (
+    <main>
+      <h1>Meal Page</h1>
+      <p>{slug}</p>
+    </main>
+  );
+};
+
+export default MealPage;
---
+++ app/meals/page.js
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+const MealsPage = () => {
+  return (
+    <main>
+      <h1>Meals Page</h1>
+      <p><Link href={'meals/meal-1'}>Meal 1</Link></p>
+      <p><Link href={'meals/meal-2'}>Meal 2</Link></p>
+      <p><Link href={'meals/meal-3'}>Meal 3</Link></p>
+    </main>
+  );
+};
+
+export default MealsPage;
