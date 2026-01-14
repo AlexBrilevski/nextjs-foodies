9ccd75143bd6479bdc855a0b5450d1da8abfeedf
Alex Brilevski <brilevski.alex@gmail.com>
2026-01-14 19:27:35 +0300

    The header section on the meals listing page was implemented

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,13 +1,26 @@
 import Link from "next/link";
 
+import classes from './page.module.css';
+
 const MealsPage = () => {
   return (
-    <main>
-      <h1>Meals Page</h1>
-      <p><Link href={'meals/meal-1'}>Meal 1</Link></p>
-      <p><Link href={'meals/meal-2'}>Meal 2</Link></p>
-      <p><Link href={'meals/meal-3'}>Meal 3</Link></p>
-    </main>
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious meals, created <span className={classes.highlight}>by you</span>
+        </h1>
+        <p>
+          Choose your favorite recipe and cook it yourself. It is easy and fun!
+        </p>
+        <p className={classes.cta}>
+          <Link href={'/share'}>
+            Share Your Favorite Recipe
+          </Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+      </main>
+    </>
   );
 };
 
