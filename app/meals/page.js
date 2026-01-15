47737bb419f7acf0cd0d28d72d313e4503caf39a
Alex Brilevski <brilevski.alex@gmail.com>
2026-01-15 20:06:50 +0300

    Fetching meals data from the database was added

--- app/meals/page.js
+++ app/meals/page.js
@@ -2,8 +2,11 @@ import Link from "next/link";
 import MealsGrid from "@/components/meals/meals-grid";
 
 import classes from './page.module.css';
+import { getMeals } from "@/lib/meals";
 
-const MealsPage = () => {
+const MealsPage = async () => {
+  const meals = await getMeals();
+
   return (
     <>
       <header className={classes.header}>
@@ -20,7 +23,7 @@ const MealsPage = () => {
         </p>
       </header>
       <main className={classes.main}>
-        <MealsGrid meals={[]} />
+        <MealsGrid meals={meals} />
       </main>
     </>
   );
