7388d938ac42769f38271d81355953b351e0f039
Alex Brilevski <brilevski.alex@gmail.com>
2026-01-17 14:36:26 +0300

    The meal details page was implemented

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -1,11 +1,36 @@
+import Image from 'next/image';
+
+import classes from './page.module.css';
+import { getMeal } from '@/lib/meals';
+
 const MealDetailsPage = async ({ params }) => {
   const { mealSlug } = await params;
+  const meal = await getMeal(mealSlug);
+
+  meal.instructions = meal.instructions.replace(/\n/g, '<br/>');
 
   return (
-    <main>
-      <h1>Meal Details</h1>
-      <p>{mealSlug}</p>
-    </main>
+    <>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image src={meal.image} alt={meal.title} fill />
+        </div>
+        <div className={classes.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={classes.creator}>
+            by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p className={classes.summary}>
+            {meal.summary}
+          </p>
+        </div>
+      </header>
+      <main>
+        <p className={classes.instructions}
+          dangerouslySetInnerHTML={{ __html: meal.instructions }}></p>
+        <p>{mealSlug}</p>
+      </main>
+    </>
   );
 };
 
