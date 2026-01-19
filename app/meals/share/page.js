91025ef474cc991ec48737a0dc8a73d34244360f
Alex Brilevski <brilevski.alex@gmail.com>
2026-01-19 20:51:48 +0300

    The Share Meal action was moved to a separate file designated for server actions

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,22 +1,8 @@
 import ImagePicker from '@/components/meals/image-picker';
 import classes from './page.module.css';
+import { shareMealAction } from '@/lib/actions';
 
 const ShareMealPage = () => {
-  async function shareMealAction(formData) {
-    'use server';
-
-    const meal = {
-      title: formData.get('title'),
-      image: formData.get('image'),
-      summary: formData.get('summary'),
-      instructions: formData.get('instructions'),
-      creator: formData.get('name'),
-      creator_email: formData.get('email'),
-    };
-
-    console.log(meal);
-  }
-
   return (
     <>
       <header className={classes.header}>
